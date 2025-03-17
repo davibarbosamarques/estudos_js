2db79a440f247e97926d5a9c2dea57bd1cf6de47
davibarbosamarques <dbarbosamarques100@gmail.com>
2025-03-17 11:49:39 -0300

    entendendo conversão

---
+++ conversao/script.js
@@ -0,0 +1,5 @@
+console.log(typeof Number("5"))
+console.log(typeof String(3))
+
+console.log(new Date().toISOString())
+console.log(new Date().toString())
---
+++ conversao/script2.js
@@ -0,0 +1,8 @@
+var name = "Davi Barbosa Marques"
+
+console.log(name.split(" "))
+
+for(const v of name){
+    console.log(v)
+}
+console.log(name.length)
